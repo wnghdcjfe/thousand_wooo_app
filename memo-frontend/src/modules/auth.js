fd00b4853e4366bb124cb48b0edfcc5323499362
wnghdcjfe <jhc9639@naver.com>
2019-11-23 22:40:47 +0900

    add container

--- memo-frontend/src/modules/auth.js
+++ memo-frontend/src/modules/auth.js
@@ -1,12 +1,34 @@
 import {createAction, handleActions} from 'redux-actions';
+import produce from 'immer';
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
-export const sampleAction = createAction(SAMPLE_ACTION);
-const initialState = {};
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+export const changeFiled = createAction(
+    CHANGE_FIELD, 
+    ({form, key, value}) =>({
+        form, key, value
+    })
+)
+export const initializeForm = createAction(INITIALIZE_FORM, form => form);
+
+const initialState = {
+    register : {
+        username : '', 
+        password : '', 
+        passwordConfirm : ''
+    }, 
+    login :{
+        username : "", 
+        password : ""
+    }
+}
+
 const auth = handleActions(
     {
-        [SAMPLE_ACTION] : (state, action) => state, 
+        [CHANGE_FIELD] : (state, {payload : {form, key, value}}) => produce(state, draft => { draft[form][key] = value}),
+        [INITIALIZE_FORM] : (state, {payload : form}) => ({...state, [form] : initialState[form]})
     }, 
     initialState
-);
+) 
 export default auth;
